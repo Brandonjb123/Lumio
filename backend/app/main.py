29273744b323cf90a955eb2a484497a3e9de08c9
Brandonjb123 <bocibatas2016@gmail.com>
2026-07-02 21:13:10 +0700

    fix: allow all origins for CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,17 +4,20 @@ from app.database import engine, Base
 from app.routers import auth, content
 from app.models.user import User
 from app.models.content import Content
+
 # Buat semua tabel di database
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Lumio API", version="0.2.0")
 
-allow_origins=[
-    "http://localhost:5173",
-    "https://lumio-six-topaz.vercel.app",
-    "https://lumio-ky9eamvxv-brandonjb-projects.vercel.app",
-    "https://*.vercel.app"
-],
+# Izinkan semua origin (sementara agar Vercel bisa akses)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Register router
 app.include_router(auth.router)
